597b01fbc013f7a4d65ca4008b2ac4cd6f0bfc5f
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-11-13 09:06:34 +0100

    rename file

--- R/02-addQuestion-module.R
+++ R/02-addQuestion-module.R
@@ -10,7 +10,7 @@ addQuestionUI <- function(id) {
         width = "100%"
       ),
     ), column(
-      2, textInput(ns("question_id"), "Input ID*", value = createRandomID())
+      2, textInput(ns("input_id"), "Input ID*", value = createRandomID())
     )),
     uiOutput(ns("help_question")),
     fluidRow(
@@ -45,9 +45,8 @@ addQuestionUI <- function(id) {
         )
       )
     ),
-    conditionalPanel(
-      ns = ns,
-      condition = "input.dependence",
+    conditionalPanel(ns = ns, condition = "input.dependence", fluidRow(column(
+      10,
       selectInput(
         ns("dependence_questions"),
         "Does this question depend on another question?",
@@ -59,7 +58,7 @@ addQuestionUI <- function(id) {
         "Value that the dependence question must take for this question to be shown",
         width = "100%"
       )
-    ),
+    ))),
     fluidRow(
       column(
         10,
@@ -72,11 +71,7 @@ addQuestionUI <- function(id) {
         ),
         uiOutput(ns("help_option"))
       ),
-      column(
-        2,
-        style = "margin-top: 1.5em;",
-        actionButton(ns("add"), "Add", width = "100%")
-      )
+      column(2, style = "margin-top: 1.5em;", actionButton(ns("add"), "Add", width = "100%"))
     )
   )
 }
@@ -90,7 +85,8 @@ addQuestionServer <- function(id, questions) {
     observe({
       logDebug("%s: Enable/Disable 'Add' button.", id)
       if (!is.null(input$question) && input$question != "" &&
-          !is.null(input$question_id) && input$question_id != "") {
+          !is.null(input$input_id) && input$input_id != "" &&
+          validOptions(input$option, input$input_type)) {
         shinyjs::enable(ns("add"), asis = TRUE)
       } else {
         shinyjs::disable(ns("add"), asis = TRUE)
@@ -108,39 +104,73 @@ addQuestionServer <- function(id, questions) {
     })
 
     observe({
-      label <- switch(input$input_type,
-                        "mc" = "Possible answers*",
-                        "select" = "Possible answers*",
-                        "text" = "Placeholder",
-                        "numeric" = "Default value",
-                        "matrix" = "Possible answers*")
-        placeholder <- switch(input$input_type,
-                              "mc" = "Option 1, Option 2, ...",
-                              "select" = "Option 1, Option 2, ...",
-                              "text" = "Enter placeholder value ...",
-                              "numeric" = "Enter default value ...",
-                              "matrix" = "Option 1, Option 2, ...")
-        value <- switch(input$input_type,
-                        "mc" = "",
-                        "select" = "",
-                        "text" = "Your Answer",
-                        "numeric" = "",
-                        "matrix" = "")
+      logDebug("%s: Update 'input$dependence_questions' value.", id)
 
-        if (input$input_type == "y/n") {
-          shinyjs::hide(ns("option"), asis = TRUE)
-        } else {
-          shinyjs::show(ns("option"), asis = TRUE)
-          updateTextInput(session, "option", label = label, placeholder = placeholder, value = value)
-        }
+      choices_df <- questions()[, c("input_id", "question")] %>%
+        distinct()
+      choices <- choices_df$input_id
+      names(choices) <- choices_df$question
+
+      updateSelectInput(
+        session,
+        "dependence_questions",
+        choices = choices
+      )
+    }) %>%
+      bindEvent(questions(),
+                ignoreInit = TRUE,
+                ignoreNULL = FALSE)
+
+    observe({
+      logDebug("%s: Hide/show 'input$option'", id)
+
+      label <- switch(
+        input$input_type,
+        "mc" = "Possible answers*",
+        "select" = "Possible answers*",
+        "text" = "Placeholder",
+        "numeric" = "Default value",
+        "matrix" = "Possible answers*"
+      )
+      placeholder <- switch(
+        input$input_type,
+        "mc" = "Option 1, Option 2, ...",
+        "select" = "Option 1, Option 2, ...",
+        "text" = "Enter placeholder value ...",
+        "numeric" = "Enter default value ...",
+        "matrix" = "Option 1, Option 2, ..."
+      )
+      value <- switch(
+        input$input_type,
+        "mc" = "",
+        "select" = "",
+        "text" = "Your Answer",
+        "numeric" = "",
+        "matrix" = ""
+      )
+
+      if (input$input_type == "y/n") {
+        shinyjs::hide(ns("option"), asis = TRUE)
+      } else {
+        shinyjs::show(ns("option"), asis = TRUE)
+        updateTextInput(
+          session,
+          "option",
+          label = label,
+          placeholder = placeholder,
+          value = value
+        )
+      }
     }) %>%
       bindEvent(input$input_type)
 
     observe({
-      # enable/disable options if input_id is reused
-      if (input$input_type == "matrix" && input$question_id %in% questions()$input_id) {
+      logDebug("%s: Enable/Disable 'input$option' if duplicated 'input_id'.", id)
+
+      if (input$input_type == "matrix" &&
+          input$input_id %in% questions()$input_id) {
         # get options of question
-        options <- questions()$option[questions()$input_id == input$question_id] %>%
+        options <- questions()$option[questions()$input_id == input$input_id] %>%
           unique() %>%
           paste(collapse = ", ")
         help_option("Provide a new 'Input ID' to add new options or add more 'Questions'.")
@@ -153,11 +183,13 @@ addQuestionServer <- function(id, questions) {
     })
 
     observe({
-      # enable/disable question name if input_id is reused
+      logDebug("%s: Enable/Disable 'input$question' if duplicated 'input_id'.", id)
+
       # this allows to add options to an existing question
-      if (input$input_type %in% c("mc", "select") && input$question_id %in% questions()$input_id) {
+      if (input$input_type %in% c("mc", "select") &&
+          input$input_id %in% questions()$input_id) {
         # get name of question
-        question <- questions()$question[questions()$input_id == input$question_id] %>%
+        question <- questions()$question[questions()$input_id == input$input_id] %>%
           unique()
         help_question("Provide a new 'Input ID' to add a new 'Question' or add more options.")
         updateTextInput(session, "question", value = question)
@@ -170,7 +202,8 @@ addQuestionServer <- function(id, questions) {
 
     observe({
       logDebug("%s: Add new question.", id)
-      if (input$question_id %in% questions()$input_id &&
+
+      if (input$input_id %in% questions()$input_id &&
           input$input_type %in% c("text", "numeric", "y/n")) {
         showNotification("Question ID already exists. Please enter a different ID.",
                          duration = 5)
@@ -181,7 +214,7 @@ addQuestionServer <- function(id, questions) {
         mutate(
           question = input$question,
           input_type = input$input_type,
-          input_id = input$question_id,
+          input_id = input$input_id,
           dependence = ifelse(input$dependence, input$dependence_questions, NA),
           dependence_value = ifelse(input$dependence, input$dependence_value, NA),
           required = input$required
@@ -192,7 +225,7 @@ addQuestionServer <- function(id, questions) {
       # clean inputs if text or numeric or y/n
       if (input$input_type %in% c("text", "numeric", "y/n")) {
         updateTextInput(session, "question", value = "")
-        updateTextInput(session, "question_id", value = createRandomID())
+        updateTextInput(session, "input_id", value = createRandomID())
         updateTextInput(session, "option", value = "")
       }
 
@@ -212,9 +245,23 @@ addQuestionServer <- function(id, questions) {
   })
 }
 
-createRandomID <- function(length = 10) {
+validOptions <- function(option, input_type) {
+  if (input_type %in% c("text", "numeric", "y/n")) {
+    return(TRUE)
+  }
+
+  if (input_type %in% c("mc", "select", "matrix")) {
+    if (is.null(option) || option == "") {
+      return(FALSE)
+    }
+  }
+
+  return(TRUE)
+}
+
+createRandomID <- function(length = 8) {
   # Define the characters to sample from
-  chars <- c(letters, LETTERS, 0:9)
+  chars <- c(letters, LETTERS) #, 0:9)
 
   # Sample characters to form the ID
   random_id <- paste0(sample(chars, length, replace = TRUE), collapse = "")
